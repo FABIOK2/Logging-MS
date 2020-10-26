11f4ed290f1a9a575b281afd8d978902275d1f0f
Martim <martimviana98@gmail.com>
2020-10-26 17:04:32 +0000

    code index.js

--- Logging-MS/index.js
+++ Logging-MS/index.js
@@ -2,10 +2,18 @@ const express = require('express')
 const app = express();
 const port = 8080;
 
+// component imports
+import { process_request } from './components/process_request.js';
+import { send_response } from './components/send_response.js';
+import { log } from './components/log.js'
+
+/* Fires when client accesses via link */
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+	log_content = process_request(req);
+	log_feedback = log(log_content)
+	res.send(log(log_feedback))
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
+	console.log(`Logging app listening on port ${port}!`)
 });
